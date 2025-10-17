31b25fc699337f3d874c62333aa6d20924097147
EriickHenriique <EriickHenriique.s.silva@gmail.com>
2025-10-17 00:29:59 -0300

    feat: camada staging feita

--- northwind/models/staging/stg_orders.sql
+++ northwind/models/staging/stg_orders.sql
@@ -1,1 +1,17 @@
-select * from orders o 
+{{ config(
+    schema='silver',
+    materialized='view'
+) }}
+
+select
+        order_id,
+        customer_id,
+        employee_id,
+        ship_via as shipper_id,
+        order_date,
+        required_date,
+        shipped_date,
+        ship_name,
+        ship_city,
+        ship_country
+from {{ref('raw_orders')}}
--- northwind/models/staging/stg_products.sql
+++ northwind/models/staging/stg_products.sql
@@ -1,1 +1,17 @@
-select  * from products p 
+{{ config(
+    schema='silver',
+    materialized='view'
+) }}
+
+
+select
+        product_id,
+        supplier_id,
+        category_id,
+        product_name,
+        unit_price,
+        units_in_stock,
+        units_on_order,
+        reorder_level,
+        discontinued
+from {{ref('raw_products')}}
